ea031466bbadbdb6e6c172254381cfe53219fb6c
nialliv <artemjevnikita@yandex.ru>
2022-05-22 13:41:50 +0400

    added additional fields

--- src/main/java/ru/kit/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/kit/springcourse/dao/PersonDAO.java
@@ -1,6 +1,5 @@
 package ru.kit.springcourse.dao;
 
-import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,37 +9,40 @@ import ru.kit.springcourse.models.Person;
 
 @Component
 public class PersonDAO {
-	private static int PEOPLE_COUNT;
-	private List<Person> people;
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
 
-	{
-		people = new ArrayList<>();
+    {
+        people = new ArrayList<>();
 
-		people.add(new Person(++PEOPLE_COUNT, "Tom"));
-		people.add(new Person(++PEOPLE_COUNT, "Bob"));
-		people.add(new Person(++PEOPLE_COUNT, "Mike"));
-		people.add(new Person(++PEOPLE_COUNT, "Katy"));
-	}
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
+    }
 
-	public List<Person> index() {
-		return people;
-	}
+    public List<Person> index() {
+        return people;
+    }
 
-	public Person show(int id) {
-		return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-	}
-	
-	public void save(Person person) {
-		person.setId(++PEOPLE_COUNT);
-		people.add(person);
-	}
-	
-	public void update(int id, Person person) {
-		Person personToBeUpdated = show(id);
-		personToBeUpdated.setName(person.getName());
-	}
-	
-	public void delete(int id) {
-		people.removeIf(p -> p.getId() == id);
-	}
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
--- src/main/java/ru/kit/springcourse/models/Person.java
+++ src/main/java/ru/kit/springcourse/models/Person.java
@@ -1,31 +1,51 @@
 package ru.kit.springcourse.models;
 
 public class Person {
-	private int id;
-	private String name;
+    private int id;
+    private String name;
+    private int age;
+    private String email;
 
-	public Person(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+    public Person() {
 
-	public int getId() {
-		return id;
-	}
+    }
 
-	public Person() {
-	}
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
